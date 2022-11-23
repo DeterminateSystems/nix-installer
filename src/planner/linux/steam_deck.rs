265d2beb7461e97279123ec3799dceb967e604d0
Ana Hobden <operator@hoverbear.org>
2022-11-23 07:00:03 -0800

    Fixup persistence path

--- src/planner/linux/steam_deck.rs
+++ src/planner/linux/steam_deck.rs
@@ -191,7 +191,7 @@ impl Planner for SteamDeck {
         Ok(InstallPlan {
             planner: Box::new(self.clone()),
             actions: vec![
-                Box::new(CreateDirectory::plan("/home/nix", None, None, 0o0755, true).await?),
+                Box::new(CreateDirectory::plan(&persistence, None, None, 0o0755, true).await?),
                 Box::new(nix_directory_unit),
                 Box::new(create_bind_mount_unit),
                 Box::new(ensure_symlinked_units_resolve_unit),
