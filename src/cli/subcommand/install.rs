830a0a68a509b43972042d6c1f623563b25fe35a
Ana Hobden <operator@hoverbear.org>
2022-12-05 11:14:50 -0800

    Add successful install message

--- src/cli/subcommand/install.rs
+++ src/cli/subcommand/install.rs
@@ -12,6 +12,7 @@ use crate::{
 };
 use clap::{ArgAction, Parser};
 use eyre::{eyre, WrapErr};
+use owo_colors::OwoColorize;
 
 /// Execute an install (possibly using an existing plan)
 ///
@@ -138,6 +139,20 @@ impl CommandExecute for Install {
                 return Err(error);
             }
         }
+
+        println!(
+            "\
+            {success}\n\
+            To use Nix, open a new terminal or run `{shell_reminder}`\n\
+            \n",
+            success = "Nix was installed successfully!".green().bold(),
+            shell_reminder = match std::env::var("SHELL") {
+                Ok(val) if val.contains("fish") =>
+                    ". /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish".bold(),
+                Ok(_) | Err(_) =>
+                    ". /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh".bold(),
+            },
+        );
 
         Ok(ExitCode::SUCCESS)
     }
