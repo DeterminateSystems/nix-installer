711564d1c705441ef170cfd8fb8c933275f6f34b
Cole Helbling <cole.helbling@determinate.systems>
2023-01-09 12:02:00 -0800

    upload_s3: provide git ish as argument
    $GITHUB_SHA isn't the actual PR's HEAD's SHA, but a merge commit or
    something.

--- upload_s3.sh
+++ upload_s3.sh
@@ -1,27 +1,28 @@
 set -eu
 
 # If the revision directory has already been created in S3 somehow, we don't want to reupload
-if aws s3 ls "$AWS_BUCKET"/"$GITHUB_SHA"/; then
-  echo "Revision $GITHUB_SHA was already uploaded; exiting"
+if aws s3 ls "$AWS_BUCKET"/"$GIT_ISH"/; then
+  echo "Revision $GIT_ISH was already uploaded; exiting"
   exit 1
 fi
 
 sudo chown $USER: -R artifacts/
 
 DEST="$1"
+GIT_ISH="$2"
 
-mkdir "$GITHUB_SHA"
+mkdir "$GIT_ISH"
 
-sed -i "s@https://install.determinate.systems/nix@https://install.determinate.systems/nix/rev/$GITHUB_SHA@" nix-installer.sh
-cp nix-installer.sh "$GITHUB_SHA"/
+sed -i "s@https://install.determinate.systems/nix@https://install.determinate.systems/nix/rev/$GIT_ISH@" nix-installer.sh
+cp nix-installer.sh "$GIT_ISH"/
 
 for artifact in $(find artifacts/ -type f); do
   chmod +x "$artifact"
-  cp "$artifact" "$GITHUB_SHA"/
+  cp "$artifact" "$GIT_ISH"/
 done
 
 # If any artifact already exists in S3 and the hash is the same, we don't want to reupload
-for file in $(find "$GITHUB_SHA" -type f); do
+for file in $(find "$GIT_ISH" -type f); do
   artifact_path="$DEST"/"$(basename "$artifact")"
   md5="$(md5sum "$artifact" | cut -d' ' -f1)"
   obj="$(aws s3api head-object --bucket "$AWS_BUCKET" --key "$artifact_path")"
@@ -33,5 +34,5 @@ for file in $(find "$GITHUB_SHA" -type f); do
   fi
 done
 
-aws s3 sync "$GITHUB_SHA"/ s3://"$AWS_BUCKET"/"$GITHUB_SHA"/ --acl public-read
-aws s3 sync s3://"$AWS_BUCKET"/"$GITHUB_SHA"/ s3://"$AWS_BUCKET"/"$DEST"/ --acl public-read
+aws s3 sync "$GIT_ISH"/ s3://"$AWS_BUCKET"/"$GIT_ISH"/ --acl public-read
+aws s3 sync s3://"$AWS_BUCKET"/"$GIT_ISH"/ s3://"$AWS_BUCKET"/"$DEST"/ --acl public-read
