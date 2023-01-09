cf7e4504462c009a9113d3ac7c27610d32f5914e
Ana Hobden <operator@hoverbear.org>
2023-01-09 11:42:15 -0800

    Fixup upload_s3.sh

--- upload_s3.sh
+++ upload_s3.sh
@@ -24,7 +24,7 @@ done
 for file in $(find "$GITHUB_SHA" -type f); do
   artifact_path="$DEST"/"$(basename "$artifact")"
   md5="$(md5sum "$artifact" | cut -d' ' -f1)"
-  obj="$(aws s3api head-object --bucket "$AWS_BUCKET" --key "$artifact_path")"
+  obj="$(aws s3api head-object --bucket "$AWS_BUCKET" --key "$artifact_path" || echo '{}')"
   obj_md5="$(jq -r .ETag <<<"$obj" | jq -r)" # head-object call returns ETag quoted, so `jq -r` again to unquote it
 
   if [[ "$md5" == "$obj_md5" ]]; then
