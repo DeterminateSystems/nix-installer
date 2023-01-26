fd76bb72599225950156a84bceb1afb3039633bf
Ana Hobden <operator@hoverbear.org>
2023-01-26 10:50:59 -0800

    Add Windows WSL test script for Ubuntu

---
+++ tests/windows/test-wsl.ps1
@@ -0,0 +1,54 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+
+# 22.04 https://cloud-images.ubuntu.com/wsl/jammy/current/
+$url = "https://cloud-images.ubuntu.com/wsl/jammy/current/ubuntu-jammy-wsl-amd64-wsl.rootfs.tar.gz"
+$File = "ubuntu-jammy-wsl-amd64-wsl.rootfs.tar.gz"
+$Name = "ubuntu-jammy"
+
+
+$TemporaryDirectory = "$HOME/nix-installer-wsl-tests-temp"
+$Image = "$TemporaryDirectory\$File"
+if (!(Test-Path -Path $Image)) {
+    Write-Output "Fetching $File to $Image..."
+    New-Item $TemporaryDirectory -ItemType Directory | Out-Null
+    Invoke-WebRequest -Uri "https://cloud-images.ubuntu.com/wsl/jammy/current/ubuntu-jammy-wsl-amd64-wsl.rootfs.tar.gz" -OutFile $Image
+} else {
+    Write-Output "Found existing $Image..."
+}
+
+$DistroName = "nix-installer-test-$Name"
+$InstallRoot = "$TemporaryDirectory\wsl-$Name"
+Write-Output "Creating WSL distribution $DistroName from $Image at $InstallRoot..."
+wsl --import $DistroName $InstallRoot $Image
+if ($LastExitCode -ne 0) {
+    exit $LastExitCode 
+}
+
+Write-Output "Preparing $DistroName for nix-installer..."
+wsl --distribution $DistroName bash --login -c "apt update --quiet"
+if ($LastExitCode -ne 0) {
+    exit $LastExitCode 
+}
+wsl --distribution $DistroName bash --login -c "apt install --quiet --yes curl build-essential"
+if ($LastExitCode -ne 0) {
+    exit $LastExitCode 
+}
+wsl --distribution $DistroName bash --login -c "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --quiet"
+if ($LastExitCode -ne 0) {
+    exit $LastExitCode 
+}
+
+Write-Output "Building and runnings nix-installer in $DistroName..."
+Copy-Item -Recurse "$PSScriptRoot\..\.." -Destination "\\wsl$\$DistroName\nix-installer"
+wsl --distribution $DistroName bash --login -c "/root/.cargo/bin/cargo run --quiet --manifest-path /nix-installer/Cargo.toml -- install linux-multi --no-confirm --init none"
+if ($LastExitCode -ne 0) {
+    exit $LastExitCode 
+}
+
+Write-Output "Testing installed Nix on $DistroName..."
+wsl --distribution $DistroName bash --login -c "nix run nixpkgs#hello"
+if ($LastExitCode -ne 0) {
+    exit $LastExitCode 
+}
