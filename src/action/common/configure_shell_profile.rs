62d8c8c5be997dd1d03f53a742db67e373be4122
Ana Hobden <operator@hoverbear.org>
2023-02-17 09:49:28 -0800

    Set the correct permissions on the zshenv

--- src/action/common/configure_shell_profile.rs
+++ src/action/common/configure_shell_profile.rs
@@ -70,7 +70,7 @@ impl ConfigureShellProfile {
                         profile_target_path,
                         None,
                         None,
-                        None,
+                        0o0755,
                         shell_buf.to_string(),
                         create_or_insert_into_file::Position::Beginning,
                     )
