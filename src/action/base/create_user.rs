344d1baccc6ece4aec58cebb7761058d81444e8a
Ana Hobden <operator@hoverbear.org>
2023-02-21 10:56:09 -0800

    Nit in error message

--- src/action/base/create_user.rs
+++ src/action/base/create_user.rs
@@ -258,7 +258,7 @@ impl Action for CreateUser {
                     Some(40) if stderr.contains("-14120") => {
                         // The user is on an ephemeral Mac, like detsys uses
                         // These Macs cannot always delete users, as sometimes there is no graphical login
-                        tracing::warn!("Encountered an exit code 40 with -14120 error while removing user, this is likely because the initial executing user did not have a secure token, or that there was no graphical login session. To delete the user, log in graphically, then in a shell (which may be over ssh) run `/usr/bin/dscl . -delete /Users/{name}");
+                        tracing::warn!("Encountered an exit code 40 with -14120 error while removing user, this is likely because the initial executing user did not have a secure token, or that there was no graphical login session. To delete the user, log in graphically, then run `/usr/bin/dscl . -delete /Users/{name}");
                     },
                     status => {
                         let command_str = format!("{:?}", command.as_std());
