f4a0a2f39281da58339cb6a5969a9589553d2d6a
Ana Hobden <operator@hoverbear.org>
2023-03-14 10:52:27 -0700

    Update both zshenv and zshrc

--- src/action/common/configure_shell_profile.rs
+++ src/action/common/configure_shell_profile.rs
@@ -34,8 +34,11 @@ const PROFILE_FISH_CONFD_PREFIXES: &[&str] = &[
 const PROFILE_TARGETS: &[&str] = &[
     "/etc/bashrc",
     "/etc/profile.d/nix.sh",
-    "/etc/zshenv",
     "/etc/bash.bashrc",
+    // https://zsh.sourceforge.io/Intro/intro_3.html
+    "/etc/zshrc",
+    "/etc/zsh/zshrc",
+    "/etc/zshenv",
     "/etc/zsh/zshenv",
 ];
 const PROFILE_NIX_FILE_SHELL: &str = "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh";
