74eb478995dd8698b3457e31123600ea30a5ee13
Ana Hobden <operator@hoverbear.org>
2023-03-29 09:48:46 -0700

    Add a bit of issue metadata

--- src/bin/nix-installer.rs
+++ src/bin/nix-installer.rs
@@ -5,14 +5,18 @@ use nix_installer::cli::CommandExecute;
 
 #[tokio::main]
 async fn main() -> eyre::Result<ExitCode> {
-    color_eyre::config::HookBuilder::default()
+    let hook_builder = color_eyre::config::HookBuilder::default()
         .issue_url(concat!(env!("CARGO_PKG_REPOSITORY"), "/issues/new"))
+        .add_issue_metadata("version", env!("CARGO_PKG_VERSION"))
+        .add_issue_metadata("os", std::env::consts::OS)
+        .add_issue_metadata("arch", std::env::consts::ARCH)
         .theme(if !atty::is(atty::Stream::Stderr) {
             color_eyre::config::Theme::new()
         } else {
             color_eyre::config::Theme::dark()
-        })
-        .install()?;
+        });
+
+    hook_builder.install()?;
 
     let cli = nix_installer::cli::NixInstallerCli::parse();
 
