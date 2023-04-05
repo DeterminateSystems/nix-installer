1718e0c42879da7e2578d2243e61b48d7259c6ab
Ana Hobden <operator@hoverbear.org>
2023-04-05 11:13:24 -0700

    Trim fdesetup output, remove accidently committed SSL related settings (#403)
    * Trim fdesetup output, remove accidently committed SSL related settings
    
    * More complete revert of accidental code
    
    * Fix a bad

--- src/settings.rs
+++ src/settings.rs
@@ -232,7 +232,6 @@ impl CommonSettings {
         let url;
         let nix_build_user_prefix;
         let nix_build_user_id_base;
-        let ssl_cert_file;
 
         use target_lexicon::{Architecture, OperatingSystem};
         match (Architecture::host(), OperatingSystem::host()) {
@@ -241,21 +240,18 @@ impl CommonSettings {
                 url = NIX_X64_64_LINUX_URL;
                 nix_build_user_prefix = "nixbld";
                 nix_build_user_id_base = 30000;
-                ssl_cert_file = None;
             },
             #[cfg(target_os = "linux")]
             (Architecture::X86_32(_), OperatingSystem::Linux) => {
                 url = NIX_I686_LINUX_URL;
                 nix_build_user_prefix = "nixbld";
                 nix_build_user_id_base = 30000;
-                ssl_cert_file = None;
             },
             #[cfg(target_os = "linux")]
             (Architecture::Aarch64(_), OperatingSystem::Linux) => {
                 url = NIX_AARCH64_LINUX_URL;
                 nix_build_user_prefix = "nixbld";
                 nix_build_user_id_base = 30000;
-                ssl_cert_file = None;
             },
             #[cfg(target_os = "macos")]
             (Architecture::X86_64, OperatingSystem::MacOSX { .. })
@@ -263,7 +259,6 @@ impl CommonSettings {
                 url = NIX_X64_64_DARWIN_URL;
                 nix_build_user_prefix = "_nixbld";
                 nix_build_user_id_base = 300;
-                ssl_cert_file = Some("/etc/ssl/certs/ca-certificates.crt".into());
             },
             #[cfg(target_os = "macos")]
             (Architecture::Aarch64(_), OperatingSystem::MacOSX { .. })
@@ -271,7 +266,6 @@ impl CommonSettings {
                 url = NIX_AARCH64_DARWIN_URL;
                 nix_build_user_prefix = "_nixbld";
                 nix_build_user_id_base = 300;
-                ssl_cert_file = Some("/etc/ssl/certs/ca-certificates.crt".into());
             },
             _ => {
                 return Err(InstallSettingsError::UnsupportedArchitecture(
@@ -291,7 +285,7 @@ impl CommonSettings {
             proxy: Default::default(),
             extra_conf: Default::default(),
             force: false,
-            ssl_cert_file,
+            ssl_cert_file: Default::default(),
             #[cfg(feature = "diagnostics")]
             diagnostic_endpoint: Some("https://install.determinate.systems/nix/diagnostic".into()),
         })
