c54743ec9d472dabb1df6a6b729a024174ce2ee9
Ana Hobden <operator@hoverbear.org>
2023-07-05 09:05:15 -0700

    Remove atty dependency

--- src/bin/nix-installer.rs
+++ src/bin/nix-installer.rs
@@ -1,4 +1,4 @@
-use std::process::ExitCode;
+use std::{io::IsTerminal, process::ExitCode};
 
 use clap::Parser;
 use nix_installer::cli::CommandExecute;
@@ -10,7 +10,7 @@ async fn main() -> eyre::Result<ExitCode> {
         .add_issue_metadata("version", env!("CARGO_PKG_VERSION"))
         .add_issue_metadata("os", std::env::consts::OS)
         .add_issue_metadata("arch", std::env::consts::ARCH)
-        .theme(if !atty::is(atty::Stream::Stderr) {
+        .theme(if !std::io::stderr().is_terminal() {
             color_eyre::config::Theme::new()
         } else {
             color_eyre::config::Theme::dark()
