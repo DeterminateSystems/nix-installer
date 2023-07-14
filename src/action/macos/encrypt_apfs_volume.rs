47d666ddb288f686959e9e74a5dc22b366b5b882
Ana Hobden <operator@hoverbear.org>
2023-07-14 14:27:28 -0700

    Spelling

--- src/action/macos/encrypt_apfs_volume.rs
+++ src/action/macos/encrypt_apfs_volume.rs
@@ -257,7 +257,7 @@ impl Action for EncryptApfsVolume {
 
 #[derive(thiserror::Error, Debug)]
 pub enum EncryptApfsVolumeError {
-    #[error("The keychain has an existing password for a non-existing \"{0}\" volume on disk `{1}`, consider removing the password with `security delete-generic-password  -a \"{0}\" -s \"Nix Store\" -l \"{1} encryption password\" -D \"Encrypted volume password\"`. Note that it's possible to have several passwords stored, so you may need to run this command several times until recieving the message `The specified item could not be found in the keychain.`")]
+    #[error("The keychain has an existing password for a non-existing \"{0}\" volume on disk `{1}`, consider removing the password with `security delete-generic-password  -a \"{0}\" -s \"Nix Store\" -l \"{1} encryption password\" -D \"Encrypted volume password\"`. Note that it's possible to have several passwords stored, so you may need to run this command several times until receiving the message `The specified item could not be found in the keychain.`")]
     ExistingPasswordFound(String, PathBuf),
     #[error("The keychain lacks a password for the already existing \"{0}\" volume on disk `{1}`, consider removing the volume with `diskutil apfs deleteVolume \"{0}\"` (if you receive error -69888, you may need to run `launchctl bootout system/org.nixos.darwin-store` and `launchctl bootout system/org.nixos.nix-daemon` first)")]
     MissingPasswordForExistingVolume(String, PathBuf),
