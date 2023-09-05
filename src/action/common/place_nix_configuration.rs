7cc9407821079763a6b184e5dc6f1085592fdb51
Ana Hobden <operator@hoverbear.org>
2023-09-05 11:39:08 -0700

    fmt

--- src/action/common/place_nix_configuration.rs
+++ src/action/common/place_nix_configuration.rs
@@ -39,8 +39,10 @@ impl PlaceNixConfiguration {
 
         let mut defaults_conf_settings = Vec::default();
         defaults_conf_settings.push(("build-users-group", nix_build_group_name));
-        defaults_conf_settings
-            .push(("experimental-features", "nix-command flakes auto-allocate-uids".into()));
+        defaults_conf_settings.push((
+            "experimental-features",
+            "nix-command flakes auto-allocate-uids".into(),
+        ));
         // https://github.com/DeterminateSystems/nix-installer/issues/449#issuecomment-1551782281
         defaults_conf_settings.push(("bash-prompt-prefix", "(nix:$name)\\040".into()));
         defaults_conf_settings.push(("extra-nix-path", "nixpkgs=flake:nixpkgs".into()));
@@ -50,7 +52,7 @@ impl PlaceNixConfiguration {
                 .map_err(|e| Self::error(ActionErrorKind::Canonicalize(ssl_cert_file, e)))?;
             defaults_conf_settings.push((
                 "ssl-cert-file = {}",
-                ssl_cert_file_canonical.display().to_string()
+                ssl_cert_file_canonical.display().to_string(),
             ));
         }
         #[cfg(not(target_os = "macos"))]
@@ -59,7 +61,11 @@ impl PlaceNixConfiguration {
         // e.g. https://github.com/NixOS/nix/issues/8444
         #[cfg(not(target_os = "macos"))]
         defaults_conf_settings.push(("auto-allocate-uids", "true".into()));
-        let defaults_conf_insert_fragment = defaults_conf_settings.iter().map(|(s, v)| format!("{s} = {v}")).collect::<Vec<_>>().join("\n");
+        let defaults_conf_insert_fragment = defaults_conf_settings
+            .iter()
+            .map(|(s, v)| format!("{s} = {v}"))
+            .collect::<Vec<_>>()
+            .join("\n");
 
         let nix_conf_insert_position = if Path::new(NIX_CONF).exists() {
             let existing_nix_conf = tokio::fs::read_to_string(NIX_CONF)
@@ -67,7 +73,7 @@ impl PlaceNixConfiguration {
                 .map_err(|e| ActionErrorKind::Read(NIX_CONF.into(), e))
                 .map_err(Self::error)?;
             tracing::trace!("Found existing `/etc/nix/nix.conf`");
-            
+
             // Find the first line that isn't just `# ...` comments
             let mut chosen_insert_after = None;
             // Warn if there seems to be a setting which is a duplicate of one we set.
@@ -76,59 +82,89 @@ impl PlaceNixConfiguration {
             for (index, line) in existing_nix_conf.lines().enumerate() {
                 let line = line.trim();
                 if line.is_empty() {
-                    continue
+                    continue;
                 } else if line.starts_with("#") {
-                    continue
+                    continue;
                 } else {
-                    chosen_insert_after = Some(Position::Before { index, expected_content: line.to_string() });
+                    chosen_insert_after = Some(Position::Before {
+                        index,
+                        expected_content: line.to_string(),
+                    });
                 }
-                
+
                 // We only scan one include of depth -- we should make this any depth, make sure to guard for loops
                 if line.starts_with("include") || line.starts_with("!include") {
                     // Need to read it in if it exists for settings
-                    let path = line.trim_start_matches("include").trim_start_matches("!include").trim();
+                    let path = line
+                        .trim_start_matches("include")
+                        .trim_start_matches("!include")
+                        .trim();
                     let relative_path = Path::new("/etc/nix/").join(path);
-                    let canonical_path = relative_path.canonicalize().map_err(|e| Self::error(ActionErrorKind::Canonicalize(relative_path, e)))?;
+                    let canonical_path = relative_path.canonicalize().map_err(|e| {
+                        Self::error(ActionErrorKind::Canonicalize(relative_path, e))
+                    })?;
                     tracing::trace!(path = %canonical_path.display(), "Reading included nix.conf");
-                    let existing_included_conf = tokio::fs::read_to_string(canonical_path)
-                        .await.ok(); // If it doesn't exist, ignore it.
+                    let existing_included_conf =
+                        tokio::fs::read_to_string(canonical_path).await.ok(); // If it doesn't exist, ignore it.
                     if let Some(existing_included_conf) = existing_included_conf {
                         let lines = existing_included_conf.lines();
                         for line in lines {
                             let mut split = line.split("=");
                             let setting_name = split.next().map(|v| v.trim());
                             let setting_value = split.next().map(|v| v.trim());
-                            if let (Some(setting_name), Some(setting_value)) = (setting_name, setting_value) {
-                                existing_conf_settings.push((setting_name.to_string(), setting_value.to_string()));
-                            } 
+                            if let (Some(setting_name), Some(setting_value)) =
+                                (setting_name, setting_value)
+                            {
+                                existing_conf_settings
+                                    .push((setting_name.to_string(), setting_value.to_string()));
+                            }
                         }
                     }
                 } else {
                     let mut split = line.split("=");
                     let setting_name = split.next().map(|v| v.trim());
                     let setting_value = split.next().map(|v| v.trim());
-                    if let (Some(setting_name), Some(setting_value)) = (setting_name, setting_value) {
-                        existing_conf_settings.push((setting_name.to_string(), setting_value.to_string()));
+                    if let (Some(setting_name), Some(setting_value)) = (setting_name, setting_value)
+                    {
+                        existing_conf_settings
+                            .push((setting_name.to_string(), setting_value.to_string()));
                     }
                 }
             }
-            tracing::trace!(existing_conf_settings = existing_conf_settings.iter().map(|(v, _)| v.to_string()).collect::<Vec<_>>().join(","), "Found existing config settings");
+            tracing::trace!(
+                existing_conf_settings = existing_conf_settings
+                    .iter()
+                    .map(|(v, _)| v.to_string())
+                    .collect::<Vec<_>>()
+                    .join(","),
+                "Found existing config settings"
+            );
 
             // Some settings (eg `experimental-features`) we must be able to set it.
             let mut required_settings = vec!["experimental-features"];
             #[cfg(not(target_os = "macos"))]
             required_settings.push("auto-allocate-uids");
             for required_setting in required_settings {
-                if let Some((existing_setting, existing_value)) = existing_conf_settings.iter().find(|(k, _v)| k == required_setting) {
-                    let required_setting = defaults_conf_settings.iter().find(|(default_setting, _v)| default_setting.starts_with(existing_setting)).expect("Required setting was not planned -- please report this");
+                if let Some((existing_setting, existing_value)) = existing_conf_settings
+                    .iter()
+                    .find(|(k, _v)| k == required_setting)
+                {
+                    let required_setting = defaults_conf_settings
+                        .iter()
+                        .find(|(default_setting, _v)| default_setting.starts_with(existing_setting))
+                        .expect("Required setting was not planned -- please report this");
                     if *existing_value != required_setting.1 {
                         return Err(ActionErrorKind::ConfigurationConflict {
                             setting: existing_setting.to_string(),
                             existing_value: existing_value.to_string(),
                             planned_value: required_setting.1.clone(),
-                        }).map_err(Self::error)
+                        })
+                        .map_err(Self::error);
                     } else {
-                        tracing::trace!("Found existing setting `{} = {existing_value}` in config, continuing", required_setting.0)
+                        tracing::trace!(
+                            "Found existing setting `{} = {existing_value}` in config, continuing",
+                            required_setting.0
+                        )
                     }
                 }
             }
@@ -137,7 +173,9 @@ impl PlaceNixConfiguration {
             for defaults_conf_setting in &defaults_conf_settings {
                 // We only set plain values so no need to be complicated.
                 for (existing_field, existing_value) in &existing_conf_settings {
-                    if defaults_conf_setting.0.trim() == *existing_field && defaults_conf_setting.1.trim() != existing_value.trim() {
+                    if defaults_conf_setting.0.trim() == *existing_field
+                        && defaults_conf_setting.1.trim() != existing_value.trim()
+                    {
                         tracing::warn!("Found existing `/etc/nix/nix.conf` setting `{existing_field} = {existing_value}` which will override a default setting from the `nix-installer`, consider unsetting it. For settings like `experimental-features` you can use an `extra-*` prefix to append to the defaults")
                     }
                 }
