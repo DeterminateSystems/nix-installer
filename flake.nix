a3b22256a2a64aa07061f73417d64ee8162229f3
Graham Christensen <graham@grahamc.com>
2023-10-11 12:10:25 -0400

    Support cargo building on a mac

--- flake.nix
+++ flake.nix
@@ -150,6 +150,7 @@
             ++ lib.optionals (pkgs.stdenv.isDarwin) (with pkgs; [
               libiconv
               darwin.apple_sdk.frameworks.Security
+              darwin.apple_sdk.frameworks.SystemConfiguration
             ])
             ++ lib.optionals (pkgs.stdenv.isLinux) (with pkgs; [
               checkpolicy
