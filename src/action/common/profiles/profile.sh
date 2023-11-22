f072444d4db3c2b972faacbce11811e5459c81b6
Graham Christensen <graham@grahamc.com>
2023-11-22 16:01:43 -0500

    Don't try to set empty keys

--- src/action/common/profiles/profile.sh
+++ src/action/common/profiles/profile.sh
@@ -1,11 +1,15 @@
 # shellcheck shell=sh
 if [ -f /nix/nix-installer ] && [ -x /nix/nix-installer ] && [ -z "${__ETC_PROFILE_NIX_SOURCED:-}" ]; then
-    NIX_INSTALLER_EXPORT_DATA=$(/nix/nix-installer export --verbose --verbose --format space-newline-separated);
-    while read -r nix_installer_export_key nix_installer_export_value; do
-        export "$nix_installer_export_key=$nix_installer_export_value";
-    done <<DATA_INPUT
-$NIX_INSTALLER_EXPORT_DATA
+    if NIX_INSTALLER_EXPORT_DATA=$(/nix/nix-installer export --format space-newline-separated); then
+        while read -r nix_installer_export_key nix_installer_export_value; do
+            if [ -n "$nix_installer_export_key" ]; then
+                export "$nix_installer_export_key=$nix_installer_export_value";
+            fi
+        done <<DATA_INPUT
+    $NIX_INSTALLER_EXPORT_DATA
 DATA_INPUT
+        unset nix_installer_export_key nix_installer_export_value
+    fi
 
-    unset NIX_INSTALLER_EXPORT_DATA nix_installer_export_key nix_installer_export_value
+    unset NIX_INSTALLER_EXPORT_DATA
 fi
