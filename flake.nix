fb4401df00bb37f0a2782eb76acb3b7865305a55
Ana Hobden <operator@hoverbear.org>
2024-01-08 08:41:39 -0800

    Release v0.16.0 (#804)

--- flake.nix
+++ flake.nix
@@ -66,7 +66,7 @@
           };
           sharedAttrs = {
             pname = "nix-installer";
-            version = "0.15.1-unreleased";
+            version = "0.16.0";
             src = builtins.path {
               name = "nix-installer-source";
               path = self;
