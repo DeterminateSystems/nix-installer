75be47fe2a88bb35336ea124184ee42f268050dd
Cole Helbling <cole.helbling@determinate.systems>
2024-07-09 07:52:31 -0700

    Preliminary support for macOS 15 beta

--- src/action/base/create_user.rs
+++ src/action/base/create_user.rs
@@ -121,86 +121,11 @@ impl Action for CreateUser {
             comment,
         } = self;
 
-        use OperatingSystem;
         match OperatingSystem::host() {
-            OperatingSystem::MacOSX {
-                major: _,
-                minor: _,
-                patch: _,
-            }
-            | OperatingSystem::Darwin => {
-                execute_command(
-                    Command::new("/usr/bin/dscl")
-                        .process_group(0)
-                        .args([".", "-create", &format!("/Users/{name}")])
-                        .stdin(std::process::Stdio::null()),
-                )
-                .await
-                .map_err(Self::error)?;
-                execute_command(
-                    Command::new("/usr/bin/dscl")
-                        .process_group(0)
-                        .args([
-                            ".",
-                            "-create",
-                            &format!("/Users/{name}"),
-                            "UniqueID",
-                            &format!("{uid}"),
-                        ])
-                        .stdin(std::process::Stdio::null()),
-                )
-                .await
-                .map_err(Self::error)?;
-                execute_command(
-                    Command::new("/usr/bin/dscl")
-                        .process_group(0)
-                        .args([
-                            ".",
-                            "-create",
-                            &format!("/Users/{name}"),
-                            "PrimaryGroupID",
-                            &format!("{gid}"),
-                        ])
-                        .stdin(std::process::Stdio::null()),
-                )
-                .await
-                .map_err(Self::error)?;
-                execute_command(
-                    Command::new("/usr/bin/dscl")
-                        .process_group(0)
-                        .args([
-                            ".",
-                            "-create",
-                            &format!("/Users/{name}"),
-                            "NFSHomeDirectory",
-                            "/var/empty",
-                        ])
-                        .stdin(std::process::Stdio::null()),
-                )
-                .await
-                .map_err(Self::error)?;
-                execute_command(
-                    Command::new("/usr/bin/dscl")
-                        .process_group(0)
-                        .args([
-                            ".",
-                            "-create",
-                            &format!("/Users/{name}"),
-                            "UserShell",
-                            "/sbin/nologin",
-                        ])
-                        .stdin(std::process::Stdio::null()),
-                )
-                .await
-                .map_err(Self::error)?;
-                execute_command(
-                    Command::new("/usr/bin/dscl")
-                        .process_group(0)
-                        .args([".", "-create", &format!("/Users/{name}"), "IsHidden", "1"])
-                        .stdin(std::process::Stdio::null()),
-                )
-                .await
-                .map_err(Self::error)?;
+            OperatingSystem::MacOSX { .. } | OperatingSystem::Darwin => {
+                create_user_macos(name, *uid, *gid)
+                    .await
+                    .map_err(Self::error)?;
             },
             _ => {
                 if which::which("useradd").is_ok() {
@@ -277,43 +202,9 @@ impl Action for CreateUser {
 
     #[tracing::instrument(level = "debug", skip_all)]
     async fn revert(&mut self) -> Result<(), ActionError> {
-        use OperatingSystem;
         match OperatingSystem::host() {
-            OperatingSystem::MacOSX {
-                major: _,
-                minor: _,
-                patch: _,
-            }
-            | OperatingSystem::Darwin => {
-                // MacOS is a "Special" case
-                // It's only possible to delete users under certain conditions.
-                // Documentation on https://it.megocollector.com/macos/cant-delete-a-macos-user-with-dscl-resolution/ and http://www.aixperts.co.uk/?p=214 suggested it was a secure token
-                // That is correct, however it's a bit more nuanced. It appears to be that a user must be graphically logged in for some other user on the system to be deleted.
-                let mut command = Command::new("/usr/bin/dscl");
-                command.args([".", "-delete", &format!("/Users/{}", self.name)]);
-                command.process_group(0);
-                command.stdin(std::process::Stdio::null());
-
-                let output = command
-                    .output()
-                    .await
-                    .map_err(|e| ActionErrorKind::command(&command, e))
-                    .map_err(Self::error)?;
-                let stderr = String::from_utf8_lossy(&output.stderr);
-                match output.status.code() {
-                    Some(0) => (),
-                    Some(40) if stderr.contains("-14120") => {
-                        // The user is on an ephemeral Mac, like detsys uses
-                        // These Macs cannot always delete users, as sometimes there is no graphical login
-                        tracing::warn!("Encountered an exit code 40 with -14120 error while removing user, this is likely because the initial executing user did not have a secure token, or that there was no graphical login session. To delete the user, log in graphically, then run `/usr/bin/dscl . -delete /Users/{}`", self.name);
-                    },
-                    _ => {
-                        // Something went wrong
-                        return Err(Self::error(ActionErrorKind::command_output(
-                            &command, output,
-                        )));
-                    },
-                }
+            OperatingSystem::MacOSX { .. } | OperatingSystem::Darwin => {
+                delete_user_macos(&self.name).await.map_err(Self::error)?;
             },
             _ => {
                 if which::which("userdel").is_ok() {
@@ -342,4 +233,131 @@ impl Action for CreateUser {
 
         Ok(())
     }
+}
+
+#[tracing::instrument(level = "debug", skip_all)]
+async fn create_user_macos(name: &str, uid: u32, gid: u32) -> Result<(), ActionErrorKind> {
+    if crate::settings::is_macos_15_or_later() {
+        execute_command(
+            Command::new("sysadminctl")
+                .process_group(0)
+                .args(["-addUser", name])
+                .args(["-UID", uid.to_string().as_ref()])
+                .args(["-GID", gid.to_string().as_ref()])
+                .arg("-roleAccount")
+                .stdin(std::process::Stdio::null()),
+        )
+        .await?;
+    } else {
+        execute_command(
+            Command::new("/usr/bin/dscl")
+                .process_group(0)
+                .args([".", "-create", &format!("/Users/{name}")])
+                .stdin(std::process::Stdio::null()),
+        )
+        .await?;
+        execute_command(
+            Command::new("/usr/bin/dscl")
+                .process_group(0)
+                .args([
+                    ".",
+                    "-create",
+                    &format!("/Users/{name}"),
+                    "UniqueID",
+                    &format!("{uid}"),
+                ])
+                .stdin(std::process::Stdio::null()),
+        )
+        .await?;
+        execute_command(
+            Command::new("/usr/bin/dscl")
+                .process_group(0)
+                .args([
+                    ".",
+                    "-create",
+                    &format!("/Users/{name}"),
+                    "PrimaryGroupID",
+                    &format!("{gid}"),
+                ])
+                .stdin(std::process::Stdio::null()),
+        )
+        .await?;
+        execute_command(
+            Command::new("/usr/bin/dscl")
+                .process_group(0)
+                .args([
+                    ".",
+                    "-create",
+                    &format!("/Users/{name}"),
+                    "NFSHomeDirectory",
+                    "/var/empty",
+                ])
+                .stdin(std::process::Stdio::null()),
+        )
+        .await?;
+        execute_command(
+            Command::new("/usr/bin/dscl")
+                .process_group(0)
+                .args([
+                    ".",
+                    "-create",
+                    &format!("/Users/{name}"),
+                    "UserShell",
+                    "/sbin/nologin",
+                ])
+                .stdin(std::process::Stdio::null()),
+        )
+        .await?;
+        execute_command(
+            Command::new("/usr/bin/dscl")
+                .process_group(0)
+                .args([".", "-create", &format!("/Users/{name}"), "IsHidden", "1"])
+                .stdin(std::process::Stdio::null()),
+        )
+        .await?;
+    }
+
+    Ok(())
+}
+
+#[tracing::instrument(level = "debug", skip_all)]
+pub async fn delete_user_macos(name: &str) -> Result<(), ActionErrorKind> {
+    if crate::settings::is_macos_15_or_later() {
+        execute_command(
+            Command::new("sysadminctl")
+                .process_group(0)
+                .args(["-deleteUser", name])
+                .stdin(std::process::Stdio::null()),
+        )
+        .await?;
+    } else {
+        // MacOS is a "Special" case
+        // It's only possible to delete users under certain conditions.
+        // Documentation on https://it.megocollector.com/macos/cant-delete-a-macos-user-with-dscl-resolution/ and http://www.aixperts.co.uk/?p=214 suggested it was a secure token
+        // That is correct, however it's a bit more nuanced. It appears to be that a user must be graphically logged in for some other user on the system to be deleted.
+        let mut command = Command::new("/usr/bin/dscl");
+        command.process_group(0);
+        command.args([".", "-delete", &format!("/Users/{}", name)]);
+        command.stdin(std::process::Stdio::null());
+
+        let output = command
+            .output()
+            .await
+            .map_err(|e| ActionErrorKind::command(&command, e))?;
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        match output.status.code() {
+            Some(0) => (),
+            Some(40) if stderr.contains("-14120") => {
+                // The user is on an ephemeral Mac, like detsys uses
+                // These Macs cannot always delete users, as sometimes there is no graphical login
+                tracing::warn!("Encountered an exit code 40 with -14120 error while removing user, this is likely because the initial executing user did not have a secure token, or that there was no graphical login session. To delete the user, log in graphically, then run `/usr/bin/dscl . -delete /Users/{}`", name);
+            },
+            _ => {
+                // Something went wrong
+                return Err(ActionErrorKind::command_output(&command, output));
+            },
+        }
+    }
+
+    Ok(())
 }
