88ffcf7ac2510b508a6d7e4c5a6c61af50116a6e
Graham Christensen <graham@grahamc.com>
2024-07-25 10:18:03 -0400

    Fixup the None variant of the determinate nixd binary path

--- src/settings.rs
+++ src/settings.rs
@@ -32,7 +32,10 @@ pub const NIX_TARBALL: &[u8] = include_bytes!(env!("NIX_INSTALLER_TARBALL_PATH")
 pub const DETERMINATE_NIXD_BINARY: Option<&[u8]> =
     Some(include_bytes!(env!("DETERMINATE_NIXD_BINARY_PATH")));
 
-#[cfg(any(not(feature = "determinate-nix"), not(target_os = "linux")))]
+#[cfg(not(all(
+    feature = "determinate-nix",
+    target_os = "linux",
+    not(target_arch = "x86"))))]
 /// The DETERMINATE_NIXD_BINARY_PATH environment variable should point to a target-appropriate
 /// static build of the Determinate Nixd binary. The contents are embedded in the resulting
 /// binary if the determinate-nix feature is turned on.
