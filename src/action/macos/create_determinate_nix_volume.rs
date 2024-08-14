e28ce91fdbba9cdea283a4bcd3505a110c3995d4
Graham Christensen <graham@grahamc.com>
2024-08-14 12:30:13 -0400

    revert inherit cmd

--- src/action/macos/create_determinate_nix_volume.rs
+++ src/action/macos/create_determinate_nix_volume.rs
@@ -187,8 +187,8 @@ impl Action for CreateDeterminateNixVolume {
 
         let mut command = Command::new("/usr/local/bin/determinate-nixd");
         command.args(["--stop-after", "mount"]);
-        command.stderr(std::process::Stdio::inherit());
-        command.stdout(std::process::Stdio::inherit());
+        command.stderr(std::process::Stdio::piped());
+        command.stdout(std::process::Stdio::piped());
         tracing::trace!(command = ?command.as_std(), "Mounting /nix");
         let output = command
             .output()
