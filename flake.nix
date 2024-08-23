1b2f42df68121aac980636034ab2ab01f0d54c56
Graham Christensen <graham@grahamc.com>
2024-08-23 11:12:07 -0400

    Update nix 2 24 4 (#1110)
    * Update nix to 2.24.4
    
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'nix':
        'https://api.flakehub.com/f/pinned/DeterminateSystems/nix/2.24.3/01916b1e-c6c2-7585-b643-4cea5c62ae8d/source.tar.gz?narHash=sha256-c/vPPkzyhoyR27RRR15UeFhpPU0jxwE%2BwHZqhTVEBKU%3D' (2024-08-19)
      → 'github:DeterminateSystems/nix/b13a7d7ef2be6cb870a19c296dc8a59724a2440a?narHash=sha256-mSYGU5vXXrrfSAailue%2BUgWNxfnL7AOVZ4RCdPK2mtM%3D' (2024-08-23)
    • Updated input 'nix/nix':
        'https://api.flakehub.com/f/pinned/NixOS/nix/2.24.3/01916a40-026a-7371-9051-47b85f686e23/source.tar.gz?narHash=sha256-aBuGXm0UwDekCYLl7xDyw%2BBAJOg7728i57TbSXzPacc%3D' (2024-08-17)
      → 'https://api.flakehub.com/f/pinned/NixOS/nix/2.24.4/01917f97-644e-756d-93f3-051d3e3b9817/source.tar.gz?narHash=sha256-oYu/9u8ht34JOTV%2BG/l3CCFJokPiUA2D8CiLZFX61PA%3D' (2024-08-23)
    • Updated input 'nix/nix/flake-compat':
        'github:edolstra/flake-compat/35bb57c0c8d8b62bbfd284272c928ceb64ddbde9?narHash=sha256-4gtG9iQuiKITOjNQQeQIpoIB6b16fm%2B504Ch3sNKLd8%3D' (2023-01-17)
      → 'github:edolstra/flake-compat/0f9255e01c2351cc7d116c072cb317785dd33b33?narHash=sha256-kvjfFW7WAETZlt09AgDn1MrtKzP7t90Vf7vypd3OL1U%3D' (2023-10-04)
    • Updated input 'nix/nix/flake-parts':
        'github:hercules-ci/flake-parts/9126214d0a59633752a136528f5f3b9aa8565b7d?narHash=sha256-sB4SWl2lX95bExY2gMFG5HIzvva5AVMJd4Igm%2BGpZNw%3D' (2024-04-01)
      → 'github:hercules-ci/flake-parts/9227223f6d922fee3c7b190b2cc238a99527bbb7?narHash=sha256-pQMhCCHyQGRzdfAkdJ4cIWiw%2BJNuWsTX7f0ZYSyz0VY%3D' (2024-07-03)
    • Updated input 'nix/nix/git-hooks-nix':
        'github:cachix/git-hooks.nix/bfef0ada09e2c8ac55bbcd0831bd0c9d42e651ba?narHash=sha256-yezvUuFiEnCFbGuwj/bQcqg7RykIEqudOy/RBrId0pc%3D' (2024-08-16)
      → 'github:cachix/git-hooks.nix/f451c19376071a90d8c58ab1a953c6e9840527fd?narHash=sha256-6FPUl7HVtvRHCCBQne7Ylp4p%2BdpP3P/OYuzjztZ4s70%3D' (2024-07-15)
    • Updated input 'nix/nix/nixpkgs':
        'github:NixOS/nixpkgs/c42fcfbdfeae23e68fc520f9182dde9f38ad1890?narHash=sha256-9tUadhnZQbWIiYVXH8ncfGXGvkNq3Hag4RCBEMUk7MI%3D' (2024-08-17)
      → 'github:NixOS/nixpkgs/c3d4ac725177c030b1e289015989da2ad9d56af0?narHash=sha256-sqLwJcHYeWLOeP/XoLwAtYjr01TISlkOfz%2BNG82pbdg%3D' (2024-08-15)
    • Updated input 'nix/nixpkgs':
        'https://api.flakehub.com/f/pinned/NixOS/nixpkgs/0.2405.631440%2Brev-2819fffa7fa42156680f0d282c60d81e8fb185b7/0190034c-678d-7039-b45c-fa38168f2500/source.tar.gz?narHash=sha256-mJi4/gjiwQlSaxjA6AusXBN/6rQRaPCycR7bd8fydnQ%3D' (2024-06-09)
      → 'https://api.flakehub.com/f/pinned/NixOS/nixpkgs/0.2405.634339%2Brev-797f7dc49e0bc7fab4b57c021cdf68f595e47841/01917ea1-8ce4-7d71-a601-f943a160def2/source.tar.gz?narHash=sha256-Qb9MhKBUTCfWg/wqqaxt89Xfi6qTD3XpTzQ9eXi3JmE%3D' (2024-08-22)
    
    * main for the moment
    
    * Use the release
    
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'nix':
        'github:DeterminateSystems/nix/b13a7d7ef2be6cb870a19c296dc8a59724a2440a?narHash=sha256-mSYGU5vXXrrfSAailue%2BUgWNxfnL7AOVZ4RCdPK2mtM%3D' (2024-08-23)
      → 'https://api.flakehub.com/f/pinned/DeterminateSystems/nix/2.24.4/01917fb6-9673-783a-9c79-5a46636fe80d/source.tar.gz?narHash=sha256-mSYGU5vXXrrfSAailue%2BUgWNxfnL7AOVZ4RCdPK2mtM%3D' (2024-08-23)

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     };
 
     nix = {
-      url = "https://flakehub.com/f/DeterminateSystems/nix/=2.24.3.tar.gz";
+      url = "https://flakehub.com/f/DeterminateSystems/nix/=2.24.4.tar.gz";
       # Omitting `inputs.nixpkgs.follows = "nixpkgs";` on purpose
     };
 
