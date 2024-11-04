0400556cc5a64c1739c2c035fd5bfe98ff8fdf8b
Cole Helbling <cole.helbling@determinate.systems>
2024-11-04 13:04:21 -0800

    fixup: get_uuid_for_label -> get_disk_info_for_label
    For a follow-up commit.

--- src/action/macos/create_volume_service.rs
+++ src/action/macos/create_volume_service.rs
@@ -13,7 +13,7 @@ use crate::action::{
     ActionTag, StatefulAction,
 };
 
-use super::get_uuid_for_label;
+use super::get_disk_info_for_label;
 
 /** Create a plist for a `launchctl` service to mount the given `apfs_volume_label` on the given `mount_point`.
  */
@@ -75,15 +75,15 @@ impl CreateVolumeService {
         if this.path.exists() {
             let discovered_plist: LaunchctlMountPlist =
                 plist::from_file(&this.path).map_err(Self::error)?;
-            match get_uuid_for_label(&this.apfs_volume_label)
+            match get_disk_info_for_label(&this.apfs_volume_label)
                 .await
                 .map_err(Self::error)?
             {
-                Some(uuid) => {
+                Some(disk_info) => {
                     let expected_plist = generate_mount_plist(
                         &this.mount_service_label,
                         &this.apfs_volume_label,
-                        uuid,
+                        disk_info.volume_uuid,
                         &this.mount_point,
                         encrypt,
                     )
@@ -191,7 +191,7 @@ impl Action for CreateVolumeService {
                 .map_err(Self::error)?;
         }
 
-        let uuid = match get_uuid_for_label(apfs_volume_label)
+        let disk_info = match get_disk_info_for_label(apfs_volume_label)
             .await
             .map_err(Self::error)?
         {
@@ -205,7 +205,7 @@ impl Action for CreateVolumeService {
         let generated_plist = generate_mount_plist(
             mount_service_label,
             apfs_volume_label,
-            uuid,
+            disk_info.volume_uuid,
             mount_point,
             *encrypt,
         )
