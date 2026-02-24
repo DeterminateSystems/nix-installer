60b4224139ecc51b401dfc6d33ede17f8125370c
Cole Helbling <cole.helbling@determinate.systems>
2026-02-23 18:58:08 -0800

    tests/vm-test/install-default: check that determinate-nixd.socket was active

--- nix/tests/vm-test/default.nix
+++ nix/tests/vm-test/default.nix
@@ -46,6 +46,18 @@ let
           exit 1
         fi
 
+        if systemctl is-active determinate-nixd.socket; then
+          echo "determinate-nixd.socket was active"
+        else
+          echo "determinate-nixd.socket was not active, should be"
+          exit 1
+        fi
+        if systemctl is-failed determinate-nixd.socket; then
+          echo "determinate-nixd.socket is failed"
+          sudo journalctl -eu determinate-nixd.socket
+          exit 1
+        fi
+
         if !(sudo systemctl start nix-daemon.service); then
           echo "nix-daemon.service failed to start"
           sudo journalctl -eu nix-daemon.service
